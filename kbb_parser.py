5b22b4d6a9126471418bc25d22740ee4a5a4e1d0
Michael <mnath10@stuy.edu>
2020-01-02 12:27:34 -0500

    I had an issue where the mpg and the horsepower conflicted and that sometimes there was more than one MPG value for a given car. I fixed it but I think there might be cases in the future where I'll have to make sure that only one MPG value is there.

--- kbb_parser.py
+++ kbb_parser.py
@@ -11,23 +11,14 @@ with open("./Test_Data/kbb.html") as file:
 relations = dict()
 
 # gets names of cars on page
-cars = soup.select("h3")
-names = [x.text.split(';')[-1].strip() for x in cars]
-for i in names:
-    # For now, have a placeholder for every key.
-    relations[i] = "0"
-rat = soup.select("div", title="KBB Expert Rating")
-rats = [x.text.split(';')[-1].strip() for x in rat]
-print(rats)
-rating = soup.find_all("div", class_="css-1hhfg7w e1q103pk0")
-for rate in rating:
-    car = rate.findChildren("h3", {"class": "css-fp7pcp e53mcov2"}, recursive=True)
-    child = rate.findChildren("div", {"class": "css-x9skgx-NumericRating e149jidm1"}, recursive=True)
-    chosen_car = ""
-    chosen_rating = ""
-    for j in car:
-        chosen_car = j.text
-    for i in child:
-        chosen_rating = i.text
-    print(chosen_car, chosen_rating)
+cars = soup.find_all("div", class_="css-1hhfg7w e1q103pk0")
+for car in cars:
+    chosen_car = car.findChildren("h3", {"class": "css-fp7pcp e53mcov2"}, recursive=True)[0].text
+    chosen_rating = car.findChildren("div", {"class": "css-x9skgx-NumericRating e149jidm1"}, recursive=True)[0].text
+    chosen_mpg = car.findChildren("div", {"class": "css-ba3sq5"})[0].findChildren("div")[0].text
+    chosen_mpg = chosen_mpg.split(" ")[0]
+    chosen_horsepower = 0
+    if chosen_car not in relations.keys():
+        relations[chosen_car] = [float(chosen_rating.strip()), chosen_mpg]
+print(relations)
 
