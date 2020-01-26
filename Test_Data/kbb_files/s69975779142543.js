3eb18229ffdb7aaa68bf5d72c911002eb67f3f5f
Michael <mnath10@stuy.edu>
2020-01-26 11:40:16 -0500

    added the kbb html file

---
+++ Test_Data/kbb_files/s69975779142543.js
@@ -0,0 +1,2 @@
+if(s_c_il[1].doPostbacks)s_c_il[1].doPostbacks({"dcs_region":9,"errors":[{"code":300,"msg":"Invalid customer id 493441:"}],"stuff":[],"tid":"enRBN4FET2Y=","uuid":"55505383392296326482666785167787207995"}
+);
