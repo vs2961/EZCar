7cde1f9e593c4a811d25aa2cbbfc9de33e81a63d
vs2961 <siuleunging@gmail.com>
2020-01-26 13:24:28 -0500

    Create Bot

--- kbb_parser.py
+++ kbb_parser.py
@@ -1,33 +1,50 @@
 from bs4 import BeautifulSoup
 # import the already defined range func from Edmunds_Parser.py
-from Edmunds_Parser import combine_range
 import re
+import requests
+import time
+
+sites = ["suv", "sedan", "hatchback", "luxury", "electric", "van-minivan", "crossover", "truck", "convertible", "coupe", "hybrid", "wagon"]
+
 
 # gets html of kbb site of interest.
-with open("./Test_Data/kbb.html") as file:
-    soup = BeautifulSoup(file, features="html.parser")
-
+#with open("./Test_Data/kbb.html") as file:
+def getCars(html, site):
+    soup = BeautifulSoup(html, features="html.parser")
 # creates a dictionary of mpg, horsepower, rating for each car.
-relations = dict()
+    relations = dict()
 
 # gets names of cars on page
-cars = soup.find_all("div", class_="css-1hhfg7w e1q103pk0")
-for car in cars:
-    chosen_car = car.findChildren("h3", {"class": "css-fp7pcp e53mcov2"}, recursive=True)[0].text
-    chosen_rating = car.findChildren("div", {"class": "css-x9skgx-NumericRating e149jidm1"}, recursive=True)[0].text
-    chosen_mpg = car.findChildren("div", {"class": "css-ba3sq5"})[0].findChildren("div")[0].text.split(" ")[0]
-    try:
-        chosen_horsepower = car.findChildren("div", {"class": "css-ba3sq5"}, title="Horsepower")[0].findChildren("div")[0].text
-    except IndexError:
-        chosen_horsepower = None
+    cars = soup("a", attrs={"id": re.compile("pathLink")})
+    for car in cars:
+        chosen_car = car.findChildren("h3", {"class": "css-fp7pcp e53mcov2"}, recursive=True)[0].text
+        try:
+            chosen_rating = car.findChildren("div", {"itemprop": "reviewRating"}, recursive=True)[0].text[-4:]
+        except IndexError:
+            continue
+        try:
+            chosen_mpg = car.findChildren("div", title="Combined Fuel Economy")[0].findChildren("div")[0].text.split(" ")[0]
+        except IndexError:
+            continue
+        try:
+            chosen_horsepower = car.findChildren("div", title="Horsepower")[0].findChildren("div")[0].text
+        except IndexError:
+            continue
 
-    if chosen_car not in relations.keys():
-        relations[chosen_car] = [float(chosen_rating.strip()), chosen_mpg, chosen_horsepower]
+        if chosen_car not in relations.keys():
+            relations[chosen_car] = [chosen_rating.strip(), chosen_mpg, chosen_horsepower]
 
-# Prints a nice way to see the dictionary of all cars and their properties.
-for item, val in enumerate(relations):
-    print(val, relations[val])
-# if you want to see the html code, just uncomment the line of code below.
-# print(soup.prettify())
+    # Prints a nice way to see the dictionary of all cars and their properties.
+    out = open(f"./Parsed_Data/{site}.csv", "w")
+    out.write("Car, Rating, MPG, Horsepower\n")
+    for item, val in enumerate(relations):
+        try:
+            out.write(f"{val}, {', '.join(relations[val])}\n") 
+        except:
+            print(val, relations[val])
 
-
+for site in sites:
+    a = requests.get(f"https://www.kbb.com/{site}/").text     
+    getCars(a, site)
+    print(f"Finished {site}")
+    time.sleep(2)
