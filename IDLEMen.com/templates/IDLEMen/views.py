4cab677634e64664799397c87de3da0fa8d84e14
vs2961 <siuleunging@gmail.com>
2020-02-08 17:14:53 -0500

    Fix bugs

--- IDLEMen.com/templates/IDLEMen/views.py
+++ IDLEMen.com/templates/IDLEMen/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, Blueprint
-hello_blueprint = Blueprint('hello',__name__)
+idlemen_blueprint = Blueprint('hello',__name__)
 
-@hello_blueprint.route('/')
-@hello_blueprint.route('/hello')
+@idlemen_blueprint.route('/')
+@idlemen_blueprint.route('/hello')
 def index():
 	return render_template("index.html")
