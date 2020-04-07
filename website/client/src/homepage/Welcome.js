c478e757d391fcbba9a2d2806b465d4547e4f715
Michael <mnath10@stuy.edu>
2020-04-06 22:43:06 -0400

    rearranging the components for the home around; having difficulty with imports

---
+++ website/client/src/homepage/Welcome.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <h1>HI</h1>
+        )
+    }
+}
+
+export default Home;
+
+export class Home {
+}
