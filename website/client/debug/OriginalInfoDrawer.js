f387493da6b02a5b0ef9831f8259c72b17dcc323
Michael <mnath10@stuy.edu>
2020-04-10 18:33:13 -0400

    restructured some components and made comments on the components being used

---
+++ website/client/debug/OriginalInfoDrawer.js
@@ -0,0 +1,56 @@
+import React, {useState} from 'react';
+import {Button} from '@rmwc/button';
+// import {SimpleMenu, MenuItem} from '@rmwc/menu';
+import {Drawer, DrawerHeader, DrawerSubtitle, DrawerTitle, DrawerContent} from '@rmwc/drawer';
+import {List, ListItem, ListDivider, SimpleListItem} from '@rmwc/list';
+import {NavLink} from 'react-router-dom';
+import '@rmwc/icon/styles';
+import '@rmwc/drawer/styles';
+
+function InfoDrawer(props) {
+        console.log(props);
+        const [open, setOpen] = useState(false)
+        return (
+            <>
+                <Drawer modal open={open} onClose={() => setOpen(false)}>
+        <DrawerHeader>
+          <DrawerTitle>Options</DrawerTitle>
+          <DrawerSubtitle>Explore!</DrawerSubtitle>
+        </DrawerHeader>
+
+
+        <DrawerContent>
+          <List>
+          {/* self explanatory but each list item points to specific pages of interest */}
+            <NavLink to="/"><ListItem><b>Introduction (DEBUG)</b></ListItem></NavLink>
+            <ListDivider></ListDivider>
+            <SimpleListItem
+            graphic="account_circle"
+            text="Profile"
+            />
+
+            <ListDivider></ListDivider>
+            <SimpleListItem 
+            graphic='directions_car'
+            text="Cars"
+            />
+            <ListDivider></ListDivider>
+  
+            <NavLink to="/about">
+              <SimpleListItem
+              graphic="supervisor_account"
+              text="About"
+              />
+            </NavLink>
+          </List>
+        </DrawerContent>
+      </Drawer>
+
+      <Button  onClick={() => setOpen(!open)} raised>
+        Options
+      </Button>
+            </>
+        );
+}
+
+export default InfoDrawer;
