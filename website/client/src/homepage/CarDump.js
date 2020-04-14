fafda7f67db8c0e0d26f22398681e19e2b325bfa
vs2961 <siuleunging@gmail.com>
2020-04-14 17:38:04 -0400

    Dump data on site

--- website/client/src/homepage/CarDump.js
+++ website/client/src/homepage/CarDump.js
@@ -1,17 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {TopAppBar, TopAppBarFixedAdjust, TopAppBarRow, TopAppBarSection, TopAppBarTitle, TopAppBarActionItem} from '@rmwc/top-app-bar';
 import '@rmwc/top-app-bar/styles';
 import InfoDrawer from './InfoDrawer';
 import AppBar from '@material-ui/core/AppBar';
 import ToolBar from '@material-ui/core/Toolbar';
 import CarAppBar from './CarAppBar';
-function Welcome(props) {
+function CarDump(props) {
+
+  const [carData, updateCarData] = useState(0);
+
+  useEffect(() => {
+    fetch("/dump").then(response => response.json()).then(data => {updateCarData(data["cars"])}) 
+}, [])
+
 
  return (
   <React.Fragment>
     <CarAppBar/>
+    {carData}
   </React.Fragment>
   
   );
 }
-export default Welcome;
+export default CarDump;
--- website/client/src/routing/CarRoutes.js
+++ website/client/src/routing/CarRoutes.js
@@ -3,6 +3,8 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Welcome from "../homepage/Welcome";
 import About from "../homepage/About";
 import CarAppBar from "../homepage/CarAppBar";
+import CarDump from "../homepage/CarDump";
+
 // Describes the routes of the website. 
 function CarRoutes() {
     return(
@@ -15,6 +17,7 @@ function CarRoutes() {
                     <CarAppBar/>
                     <About/>
                 </Route>
+                <Route path={"/cars"} component={CarDump} exact/>
             </Switch>
         </BrowserRouter>
     )
