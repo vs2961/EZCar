6f2bed5339009ae2cab08e460bd0d4240314e706
Michael <mnath10@stuy.edu>
2020-04-14 21:54:20 -0400

    transferred

--- website/client/src/homepage/InfoDrawer.js
+++ website/client/src/homepage/InfoDrawer.js
@@ -6,7 +6,7 @@ import DriveEtaIcon from '@material-ui/icons/DriveEta';
 import InfoIcon from '@material-ui/icons/Info';
 import IconButton from '@material-ui/core/IconButton'
 import {makeStyles} from '@material-ui/core/styles';
-import {List, ListItem, ListItemText, Divider, Button, Drawer, ListItemAvatar, Avatar} from '@material-ui/core'
+import {List, ListItem, ListItemText, Divider, Drawer, ListItemAvatar, Avatar} from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles(
--- website/client/src/routing/CarRoutes.js
+++ website/client/src/routing/CarRoutes.js
@@ -1,8 +1,11 @@
+
 import React from "react";
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Welcome from "../homepage/Welcome";
 import About from "../homepage/About";
 import CarAppBar from "../homepage/CarAppBar";
+import CarDump from "../homepage/CarDump";
+
 // Describes the routes of the website. 
 function CarRoutes() {
     return(
@@ -15,6 +18,7 @@ function CarRoutes() {
                     <CarAppBar/>
                     <About/>
                 </Route>
+                <Route path={"/cars"} component={CarDump} exact/>
             </Switch>
         </BrowserRouter>
     )
