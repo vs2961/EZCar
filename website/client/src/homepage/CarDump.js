feeaf8ab008e1acc611e4712e1539be988623c86
Michael <mnath10@stuy.edu>
2020-04-15 19:53:36 -0400

    updated buttons on the /cars page

--- website/client/src/homepage/CarDump.js
+++ website/client/src/homepage/CarDump.js
@@ -1,9 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState} from 'react';
 import '@rmwc/top-app-bar/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import CarAppBar from './CarAppBar';
 import Button from '@material-ui/core/Button'
-function CarDump(props) {
 
+const useStyles = makeStyles({
+  root : {
+    fontWeight: 'bold',
+    padding: '1.1em',
+    margin: "1.1em",
+    backgroundColor: "#03f4fc"
+  }
+})
+function CarDump(props) {
+  const classes = useStyles();
   const [carData, updateCarData] = useState(0);
 
   useEffect(() => {
@@ -15,7 +25,12 @@ function CarDump(props) {
   <React.Fragment>
     <CarAppBar/>
     {/* {carData} */}
-    <Button variant="contained">Dump Car Data</Button>
+    {["Trucks", "SUVs", "Van-Minivans", "Hatchbacks", "Electrics", "Crossovers", "Convertibles", "Luxaries", "Wagons", "Coupes", "Sedans"]
+    .map((text, index) => <Button variant="contained" color="primary" className={classes.root} key={index}>Load {text}</Button>)}
+    {/* <Button variant="contained" color="secondary" className={classes.root}>Dump Car Data</Button>
+    <Button variant="contained" color="warning" className={classes.root}> Load SUVs</Button>
+    <Button variant="contained" color="warning" className={classes.root}> Load SUVs</Button> */}
+
   </React.Fragment>
   
   );
