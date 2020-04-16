c4486f37591f51d87ab008d2d2f33f042a8713ed
erickim0919 <58961847+erickim0919@users.noreply.github.com>
2020-04-16 17:59:26 -0400

    Add files via upload

--- Scraping/kbb_parser.py
+++ Scraping/kbb_parser.py
@@ -5,17 +5,21 @@ import requests
 import time
 
 sites = ["suv", "sedan", "hatchback", "luxury", "electric", "van-minivan", "crossover", "truck", "convertible", "coupe", "hybrid", "wagon"]
-
+matchmatch = []
 
 # gets html of kbb site of interest.
 #with open("./Test_Data/kbb.html") as file:
-def getCars(html, site):
+def getCars(html, site, out):
     soup = BeautifulSoup(html, features="html.parser")
 # creates a dictionary of mpg, horsepower, rating for each car.
     relations = dict()
+    idscrape = soup.prettify()
+    matchmatch = [match.start() for match in re.finditer("vehicleId", idscrape)]
 
 # gets names of cars on page
     cars = soup("a", attrs={"id": re.compile("pathLink")})
+    p = 0
+
     for car in cars:
         chosen_car = car.findChildren("h3", {"class": "css-fp7pcp e53mcov2"}, recursive=True)[0].text
         try:
@@ -31,20 +35,58 @@ def getCars(html, site):
         except IndexError:
             continue
 
+        chosen_price = soup.prettify()[int(matchmatch[p])+11:int(matchmatch[p])+17]
+
+        b = requests.get(f"https://www.kbb.com/vehicles/hub/_pricingmodal/?vehicleid={chosen_price}&intent=buy-new&tab=style&forceupdate=false/").text
+        soup2 =  BeautifulSoup(b, features="html.parser")
+
+        try:
+            price = soup2.findAll("div", {"id":"msrp"})[0].text
+        except IndexError:
+            continue
+
+        try:
+            marketprice = soup2.findAll("div", {"id":"fairPurchasePrice"})[0].text
+        except IndexError:
+            continue
+
+        try:
+            pricerange = soup2.findAll("div", {"class":"paragraph-two msrp-title"})
+        except IndexError:
+            continue
+
+        try:
+            imagelink = soup2.findAll("img")
+        except IndexError:
+            print("broken ooga")
+
+        imagelink = "https:"+imagelink[0]['src']
+
+        pricerange = list(map(lambda x: int(x.text[1:].replace(",", "")), pricerange))
+
+        pricemax = str(max(pricerange))
+        pricemin = str(min(pricerange))
+
         if chosen_car not in relations.keys():
-            relations[chosen_car] = [chosen_rating.strip(), chosen_mpg, chosen_horsepower]
+            relations[chosen_car] = [site, chosen_rating.strip(), chosen_mpg, chosen_horsepower, pricemin, pricemax, marketprice, imagelink]
+
+        if p%10 == 0:
+            print("ooga checkpoint")
+        p += 1
 
     # Prints a nice way to see the dictionary of all cars and their properties.
-    out = open(f"./Parsed_Data/{site}.csv", "w")
-    out.write("Car, Rating, MPG, Horsepower\n")
+
     for item, val in enumerate(relations):
         try:
-            out.write(f"{val}, {', '.join(relations[val])}\n") 
+            out.write(f"{relations[val][0]}, {val}, {', '.join(relations[val][1:])}\n")
         except:
             print(val, relations[val])
 
+out = open("./Parsed_Data/master.csv", "w")
+out.write("Type, Car, Rating, MPG, Horsepower, Min Price, Max Price, Market Price, Image Link\n")
+
 for site in sites:
-    a = requests.get(f"https://www.kbb.com/{site}/").text     
-    getCars(a, site)
+    a = requests.get(f"https://www.kbb.com/{site}/").text
+    getCars(a, site, out)
     print(f"Finished {site}")
     time.sleep(2)
