1add74eead26ac9bd4be5c3e55000376fc8817bf
erickim0919 <58961847+erickim0919@users.noreply.github.com>
2020-04-17 20:55:46 -0400

    Update seats_parser.py

--- Scraping/seats_parser.py
+++ Scraping/seats_parser.py
@@ -1,17 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
 
+#function to get the seat information of each car
 def getSeats(site, id):
+    #reverse engineers KBB's built-in comparison feature to pull seat information
     kbb = requests.get(f"https://www.kbb.com/vehicles/hub/_compare/?vehicleid={id}&category={site}/").text
-
     soup = BeautifulSoup(kbb, features="html.parser")
-    # creates a dictionary of mpg, horsepower, rating for each car.
 
+    #looks for the big div tag that contains information for car
     try:
         seats_scrape = soup.findAll("div", {"class":"compare-vehicle-col"})
     except IndexError:
         pass
-
+    
+    #looks for the specific div tag that contains the seat info
     try:
         for i in seats_scrape:
             seat_info = i.findAll("div", {"data-compare-row":"5"})
