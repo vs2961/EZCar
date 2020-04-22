57794962c30163e33997fed6981afd6a5a1f823e
vs2961 <siuleunging@gmail.com>
2020-04-22 19:34:59 -0400

    Fix the database

--- website/app/models.py
+++ website/app/models.py
@@ -2,23 +2,147 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 from app import db
 
-
 class Car(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String, nullable=False)
-    name = db.Column(db.String, nullable=False, unique=False)
-    rating = db.Column(db.Float, nullable=True)
-    mpg = db.Column(db.Integer, nullable=False)
-    horsepower = db.Column(db.Integer, nullable=False)
-    price = db.Column(db.Float, nullable=False)
+    id = db.Column(
+              db.Integer, 
+              primary_key=True, 
+              supports_json = True,
+              supports_yaml = True,
+              supports_dict = True,
+              on_serialize = None,
+              on_deserialize = None,
+              display_name = None)
+    type = db.Column(
+              db.String,
+              supports_json = True,
+              supports_yaml = True,
+              supports_dict = True,
+              on_serialize = None,
+              on_deserialize = None,
+              display_name = None)
+    name = db.Column(
+              db.String, 
+              nullable=False, 
+              unique=False, 
+              supports_json = True,
+              supports_yaml = True,
+              supports_dict = True,
+              on_serialize = None,
+              on_deserialize = None,
+              display_name = None)
+    is_electric = db.Column(
+              db.Boolean, 
+              nullable=False, 
+              unique=False, 
+              supports_json = True,
+              supports_yaml = True,
+              supports_dict = True,
+              on_serialize = None,
+              on_deserialize = None,
+              display_name = None)
+    mpg = db.Column(
+              db.Integer, 
+              nullable=False, 
+              supports_json = True,
+              supports_yaml = True,
+              supports_dict = True,
+              on_serialize = None,
+              on_deserialize = None,
+              display_name = None)
+    horsepower = db.Column(
+              db.Integer, 
+              nullable=False, 
+              supports_json = True,
+              supports_yaml = True,
+              supports_dict = True,
+              on_serialize = None,
+              on_deserialize = None,
+              display_name = None)
+    seats = db.Column(
+              db.Integer, 
+              nullable=False, 
+              supports_json = True,
+              supports_yaml = True,
+              supports_dict = True,
+              on_serialize = None,
+              on_deserialize = None,
+              display_name = None)
+    MSRP = db.Column(
+              db.Float, 
+              nullable=False, 
+              supports_json = True,
+              supports_yaml = True,
+              supports_dict = True,
+              on_serialize = None,
+              on_deserialize = None,
+              display_name = None)
+    market_price = db.Column(
+              db.Float, 
+              nullable=False, 
+              supports_json = True,
+              supports_yaml = True,
+              supports_dict = True,
+              on_serialize = None,
+              on_deserialize = None,
+              display_name = None)
+    min_price = db.Column(
+              db.Float, 
+              nullable=False, 
+              supports_json = True,
+              supports_yaml = True,
+              supports_dict = True,
+              on_serialize = None,
+              on_deserialize = None,
+              display_name = None)
+    max_price = db.Column(
+              db.Float, 
+              nullable=False, 
+              supports_json = True,
+              supports_yaml = True,
+              supports_dict = True,
+              on_serialize = None,
+              on_deserialize = None,
+              display_name = None)
+    image_link = db.Column(
+              db.String, 
+              nullable=True, 
+              supports_json = True,
+              supports_yaml = True,
+              supports_dict = True,
+              on_serialize = None,
+              on_deserialize = None,
+              display_name = None)
 
-    def __init__(self,type, name, rating, mpg, horsepower, price):
+    def __init__(self, type, name, is_electric, mpg, horsepower, seats, MSRP, market_price, min_price, max_price, image_link):
         self.type = type
         self.name = name
-        self.price = price
+        self.is_electric = is_electric
         self.mpg = mpg
         self.horsepower = horsepower
-        self.rating = rating
+        self.seats = seats
+        self.MSRP = MSRP
+        self.market_price = market_price
+        self.min_price = min_price
+        self.max_price = max_price
+        self.image_link = image_link
+
+    def serialize(self):
+        return {
+            "TYPE": self.type,
+            "NAME": self.name,
+            "IS ELECTRIC": self.is_electric,
+            "MPG": self.mpg,
+            "HORSEPOWER": self.horsepower, 
+            "SEATS": self.seats, 
+            "MSRP": self.MSRP, 
+            "MARKET PRICE": self.market_price, 
+            "PRICE_RANGE": f"{self.min_price} - {self.max_price}",
+            "IMAGE_LINK": self.image_link 
+        }
 
     def __repr__(self):
-        return "ID #{}: TYPE: {}. NAME: {}. RATING: {}. MPG: {}. HORSEPOWER: {}. PRICE: {}.".format(self.id, self.type, self.name, self.rating, self.mpg, self.horsepower, self.price)
+        return f"ID #{self.id}: TYPE: {self.type}. NAME: {self.name}. \
+                 IS_ELECTRIC: {self.is_electric}. MPG: {self.mpg}. \
+                 HORSEPOWER: {self.horsepower}. SEATS: {self.seats}. \
+                 MIN_PRICE: {self.min_price}. MAX_PRICE: {self.max_price}. \
+                 MARKET_PRICE: {self.market_price}. IMAGE_LINK: {self.image_link}"
--- website/db_create.py
+++ website/db_create.py
@@ -1,12 +1,25 @@
 from app import db
 from app.models import Car
+
 db.create_all()
 
-csv_list = [i.rstrip() for i in open("../Scraping/master.csv")]
+csv_list = open("../Scraping/master.csv")
+csv_list.readline()
 
 for i in csv_list:
-    a = i.split(",")
-    new_car = Car(a[0], a[1], float(a[2]), int(a[3]), int(a[4]), 0.0)
-    
+    a = [x.strip() for x in i.split(",")]
+    new_car = Car(a[1], 
+                  a[2],
+                  False if a[3].split(" ") == "MPG" else True,
+                  int(a[3].split(" ")[0]), 
+                  int(a[4]), 
+                  int(a[5]) if a[5] != "-" else -1, 
+                  float(a[6]),
+                  float(a[7]),
+                  int(a[8].split(" - ")[0].lstrip("$")),
+                  int(a[8].lstrip("$")) if not len(a[8].split(" - ")) > 1 else int(a[8].split(" - ")[1].lstrip("$")),
+                  a[9]
+                 )
     db.session.add(new_car)
+
 db.session.commit()
