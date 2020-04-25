ec73722ba2f07bf6a77d72b1f50e73cb8266b742
erickim0919 <jkim11@stuy.edu>
2020-04-25 19:32:09 -0400

    updated js files

--- website/client/src/homepage/CarDump.js
+++ website/client/src/homepage/CarDump.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState} from 'react';
-import '@rmwc/top-app-bar/styles';
 import {makeStyles} from '@material-ui/core/styles';
 import CarAppBar from './CarAppBar';
 import Button from '@material-ui/core/Button'
--- website/client/src/homepage/Welcome.js
+++ website/client/src/homepage/Welcome.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import '@rmwc/top-app-bar/styles';
 import CarAppBar from './CarAppBar';
 function Welcome(props) {
- 
+
  return (
   <CarAppBar/>
   );
