3e7986ec35b7d51a5fe81204b122584930bef8a6
Michael <mnath10@stuy.edu>
2020-04-26 16:39:20 -0400

    got the cards to be lined up in a row

--- website/client/src/homepage/Welcome.js
+++ website/client/src/homepage/Welcome.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import CarAppBar from './CarAppBar';
 import CCard from '../cards/CCard';
-function Welcome(props) {
+import Grid from '@material-ui/core/Grid';
+import { Button } from '@material-ui/core';
+import CSSGrid from './TestGrid';
+import {makeStyles} from '@material-ui/core/styles';
+import { sizing } from '@material-ui/system';
 
+
+function Welcome(props) {
  return (
    <>
     <CarAppBar/>
-    <div>
-      <CCard/>
-      <CCard/>
-      <CCard/>
-    </div>
+    <Grid container spacing={1} alignItems="stretch">
+      <Grid item xs >
+        <CCard/>
+      </Grid>
+      <Grid item xs >
+        <CCard/>
+      </Grid>
+      <Grid item xs>
+        <CCard/>
+      </Grid>
+    </Grid>
+
   </>
   );
 }
