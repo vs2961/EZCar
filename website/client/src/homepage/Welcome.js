102dba1a8936cd7cf8cbd8c15999b7f14ee68b94
Michael <mnath10@stuy.edu>
2020-05-14 17:48:54 -0400

    started working on sending data to backend

--- website/client/src/homepage/Welcome.js
+++ website/client/src/homepage/Welcome.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import CarAppBar from './CarAppBar';
-import CCard from '../../debug/CCard';
 import Grid from '@material-ui/core/Grid';
 import NewCard from '../cards/NewCard';
+import Button from '@material-ui/core/Button';
+import axios from 'axios';
 
 
 class Welcome extends React.Component {
@@ -19,11 +20,21 @@ class Welcome extends React.Component {
       curRound: this.rounds[0],
       curIndex: 0
     }
+
+    this.choices = {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ title: 'React POST Request Example' }),
+    Type : "SUV",
+    Price : 50000,
+    Seats : 4
+    };
     
     this.updateChoices = this.updateChoices.bind(this);
+    this.submitData = this.submitData.bind(this);
   }
 
-  updateChoices(e) {
+  updateChoices(val) {
     var updatedIndex;
     if (this.state.curIndex >= this.rounds.length - 1) updatedIndex = this.rounds.length - 1
     else updatedIndex = this.state.curIndex + 1
@@ -35,9 +46,14 @@ class Welcome extends React.Component {
     })
   }
 
+  submitData = () => {
+
+  axios.get("https://localhost:5000/dump", this.choices).then(res => console.log(res.data))
+}
   componentDidUpdate() {
-    console.log(this.state.curIndex);
-    console.log(this.state.curRound);
+    // console.log(this.state.curIndex);
+    // console.log(this.state.curRound);
+
   }
 
 
@@ -47,9 +63,10 @@ class Welcome extends React.Component {
     <CarAppBar/>
     <Grid container>
     {this.state.curRound.map((item, index) => {
-            return <Grid item xs={4} key={index}> <NewCard func={this.updateChoices}text={item}/> </Grid>
+            return <Grid item xs={4} key={index}> <NewCard val={item} func={this.updateChoices}text={item}/> </Grid>
         })}
     </Grid>
+    <Button onClick={this.submitData}>Submit Data</Button>
   </>
    );
  }
