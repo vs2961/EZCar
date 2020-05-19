b6c9b2bebb6917aa67905e704f2bf27bb6fd45a5
vs2961 <siuleunging@gmail.com>
2020-05-18 21:09:24 -0400

    Add Checking

--- website/app/views.py
+++ website/app/views.py
@@ -10,7 +10,6 @@ cars_blueprint = Blueprint(
 @cars_blueprint.route('/dump', methods=["POST"])
 def serve():
     req_data = request.get_json()
-    print(req_data)
     cars = Car.query
     if req_data["Price"]:
         cars = cars.filter(Car.max_price <= req_data["Price"][1])\
@@ -21,3 +20,37 @@ def serve():
         cars = cars.filter(Car.seats.between(req_data["Seats"][0], req_data["Seats"][1]))
     carList = [car.serialize() for car in cars]
     return jsonify(carList)
+
+@cars_blueprint.route('/available', methods=["POST"])
+def get_available():
+    req_data = request.get_json()
+    cars = Car.query
+    if req_data["Price"]:
+        cars = cars.filter(Car.max_price <= req_data["Price"][1])\
+                   .filter(Car.min_price >= req_data["Price"][0])
+    if req_data["Type"]:
+        cars = cars.filter_by(type=req_data["Type"])
+    if req_data["Seats"]:
+        cars = cars.filter(Car.seats.between(req_data["Seats"][0], req_data["Seats"][1]))
+    booleanList = []
+    for i in req_data["futureRound"]:
+        if i[0] == "Type":
+            if (cars.filter_by(type = i[2]).count() > 0):
+                booleanList.append(True)
+            else:
+                booleanList.append(False)
+        elif i[0] == "Price":
+            if (cars.filter(Car.max_price <= i[2][1])\
+                    .filter(Car.min_price >= i[2][0]).count() > 0):
+                booleanList.append(True)
+            else:
+                booleanList.append(False)
+        elif i[0] == "Seats":
+            if (cars.filter(Car.seats.between(i[2][0], i[2][1])).count() > 0):
+                booleanList.append(True)
+            else:
+                booleanList.append(False)
+
+    print(booleanList)
+    return jsonify(booleanList)
+ 
