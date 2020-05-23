60dc2b28592c0b773e829c3bf851c8f206d61fbc
vs2961 <siuleunging@gmail.com>
2020-05-23 15:59:50 -0400

    Fix Welcome.js

--- website/client/src/homepage/Welcome.js
+++ website/client/src/homepage/Welcome.js
@@ -8,45 +8,46 @@ import { ThemeProvider } from '@material-ui/core';
 
 
 class Welcome extends React.Component {
-  constructor(props) {
-    super(props);
-    this.rounds = [
-      ["Newcomer", "Family", "Exclusive"],
-      ["Convertible", "SUV", "Sports"],
-      ["Less Than 3", "Less Than 5", "More Than 5"]
-    ]
-    this.state = {
-      price: 0,
-      seats: 0,
-      curRound: this.rounds[0],
-      curIndex: 0
+    constructor(props) {
+        super(props);
+        this.rounds = [
+            [["Price", "Newcomer", [0, 20000]], ["Price", "Family Package", [20001, 45000]], ["Price", "Exclusive", [45001, Number.MAX_SAFE_INTEGER]]],
+            [["Type", "Convertible", 'convertible'], ["Type", "SUV", 'suv'], ["Type", "Sports", 'sports']],
+            [["Seats", "Less Than 3", [0,3]], ["Seats", "Less Than 5", [4,5]], ["Seats", "More Than 5", [5, "unlimited"]]]
+        ]
+        this.state = {
+            curRound: this.rounds[0],
+            curIndex: 0,
+            avails: [true, true, true]
+        }
+        this.choices = {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ title: 'User\'s Choices' }),
+            Price: null,
+            Type: null,
+            Seats: null,
+            futureRound: this.rounds[1]
+        };
+
+        this.updateChoices = this.updateChoices.bind(this);
+        this.submitData = this.submitData.bind(this);
     }
 
-    this.choices = {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ title: 'React POST Request Example' }),
-    Type : "SUV",
-    Price : 50000,
-    Seats : 4
-    };
-    
-    this.updateChoices = this.updateChoices.bind(this);
-    this.submitData = this.submitData.bind(this);
-  }
+    updateChoices(val) {
+        var updatedIndex;
+        if (this.state.curIndex >= this.rounds.length - 1) updatedIndex = this.rounds.length - 1
+        else updatedIndex = this.state.curIndex + 1
+        this.setState({
+            curIndex: updatedIndex,
+            curRound: this.rounds[updatedIndex]
+        })
 
-  updateChoices(val) {
-    var updatedIndex;
-    console.log(updatedIndex)
-    if (this.state.curIndex >= this.rounds.length - 1) updatedIndex = this.rounds.length - 1
-    else updatedIndex = this.state.curIndex + 1
-    this.setState({
-      price: 100,
-      seats: 10,
-      curIndex: updatedIndex,
-      curRound: this.rounds[updatedIndex]
-    })
-  }
+        this.choices[val[0]] = val[2]
+        this.choices['futureRound'] = this.rounds[updatedIndex]
+        console.log(this.choices);
+    }
+
     // currently in debug mode. When user is done selecting their choices, the callback fxn submitData will be auto-called
     submitData = () => {
         axios.post("/dump", this.choices).then(res => console.log(res.data))
@@ -64,31 +65,31 @@ class Welcome extends React.Component {
         })
     }
 
-  carImage(index) {
-    const carPics = [ ['newcomer.png', 'family.png', 'exclusive.png'], ['bronze.jpg','bronze.jpg','bronze.jpg'], ['bronze.jpg','bronze.jpg','bronze.jpg'] ]
-    return carPics[this.state.curIndex][index]
-  }
+    carImage(index) {
+        const carPics = [ ['newcomer.png', 'family.png', 'exclusive.png'], ['bronze.jpg','bronze.jpg','bronze.jpg'], ['bronze.jpg','bronze.jpg','bronze.jpg'] ]
+        return carPics[this.state.curIndex][index]
+    }
 
- render() { 
-   return (
-   <>
-    <CarAppBar/>
-    <Grid container>
-    {this.state.curRound.map((item, index) => {
-            return (
-                <Grid item xs={4} key={index}>
-                <NewCard val={item}
-                         func={this.updateChoices}
-                         text={item}
-                         imgName = {this.carImage(index)}/>
-                </Grid>)
-        })}
-    </Grid>
-    <Button onClick={this.submitData}>Submit Data</Button>
-    Here is what we found. . . 
-  </>
-   );
- }
+    render() { 
+        return (
+            <>
+            <CarAppBar/>
+            <Grid container>
+            {this.state.curRound.map((item, index) => {
+                return (
+                    <Grid item xs={4} key={index}>
+                    <NewCard val={item}
+                    func={this.updateChoices}
+                    text={item[1]}
+                    imgName = {this.carImage(index)}/>
+                    </Grid>)
+            })}
+            </Grid>
+            <Button onClick={this.submitData}>Submit Data</Button>
+            Here is what we found. . . 
+            </>
+        );
+    }
 }
 
 export default Welcome;
