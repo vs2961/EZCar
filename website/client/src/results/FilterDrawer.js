a7424f9b9513a93b36f9d2f71c813e3688492130
Michael <mnath10@stuy.edu>
2020-05-31 21:31:55 -0400

    the slider isn't movable yet; you must click

--- website/client/src/results/FilterDrawer.js
+++ website/client/src/results/FilterDrawer.js
@@ -7,8 +7,9 @@ import {
 	Drawer,
 	ListItemAvatar,
 	Avatar,
+	Chip,
 } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
 import SearchIcon from "@material-ui/icons/Search";
 import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
 import Slider from "@material-ui/core/Slider";
@@ -17,22 +18,22 @@ import MenuItem from "@material-ui/core/MenuItem";
 import Icon from "@material-ui/core/Icon";
 import CheckIcon from "@material-ui/icons/Check";
 
+const formatter = new Intl.NumberFormat("en-US", {
+	style: "currency",
+	currency: "USD",
+	minimumFractionDigits: 2,
+});
+
 const useStyles = makeStyles((theme) => ({
 	root: {
 		width: 500,
 	},
 }));
 
-const sliderStyles = makeStyles((theme) => ({
-	root: {
-		margin: "4vw",
-		maxWidth: 500,
-	},
-}));
 const FilterDrawer = (props) => {
 	const classes = useStyles();
-	const sliderClass = sliderStyles();
 	const [filter, setFilter] = React.useState("MSRP");
+	const [finalValue, setFinal] = React.useState(0);
 	const [open, setOpen] = React.useState(false);
 	// looks to see whether the user presses certain keys (mainly for UX)
 	const handleDrawerOpen = () => {
@@ -49,28 +50,34 @@ const FilterDrawer = (props) => {
 			label: `MIN VALUE: 0`,
 		},
 		{
-			value: 100000,
-			label: `MIN VALUE: 10000`,
+			value: props.maxAvailPrice,
+			label: `MIN VALUE: ${props.maxAvailPrice}`,
 		},
 	];
-	const filterBy = (curOption) => {
-		return (
-			<Select value={curOption} onChange={handleFilter}>
-				<MenuItem value={10}>Ten</MenuItem>
-			</Select>
-		);
-	};
 
 	const submitFilters = () => {
 		return (
-		handleDrawerClose(),
-		console.log(filter),
-		props.func(filter)
-		)
-	}
+			handleDrawerClose(), props.priceFunc(finalValue), props.func(filter)
+		);
+	};
 
 	const handleFilter = (event) => {
 		setFilter(event.target.value);
+	};
+
+	const PriceLabel = withStyles({
+		root: {
+			margin: "4vw",
+			maxWidth: 500,
+		},
+		valueLabel: {
+			background: "transparent",
+			color: "red",
+		},
+	})(Slider);
+
+	const handlePriceChange = (event, newValue) => {
+		setFinal(newValue);
 	};
 	const topics = (anchor) => (
 		<div role={"presentation"} className={classes.root}>
@@ -80,12 +87,14 @@ const FilterDrawer = (props) => {
 			{/* Just maps out the topics, making listitems for each one */}
 			<List>
 				{[
-					<Slider
-						className={sliderClass.root}
+					<PriceLabel
 						min={0}
-						max={100000}
-						color="primary"
+						max={props.maxAvailPrice}
 						marks={filterRange}
+						valueLabelDisplay="auto"
+						onChange={handlePriceChange}
+						value={finalValue}
+						valueLabelFormat={(val) => formatter.format(val)}
 					/>,
 					<Select value={filter} onChange={handleFilter}>
 						<MenuItem value="MSRP">Price</MenuItem>
@@ -109,12 +118,12 @@ const FilterDrawer = (props) => {
 	);
 	return (
 		<div>
-				<IconButton edge="end" onClick={handleDrawerOpen}>
-					<SearchIcon />
-				</IconButton>
-				<Drawer variant="persistent" anchor={"right"} open={open}>
-					{topics("right")}
-				</Drawer>
+			<IconButton edge="end" onClick={handleDrawerOpen}>
+				<SearchIcon />
+			</IconButton>
+			<Drawer variant="persistent" anchor={"right"} open={open}>
+				{topics("right")}
+			</Drawer>
 		</div>
 	);
 };
--- website/client/src/results/ResultPage.js
+++ website/client/src/results/ResultPage.js
@@ -16,30 +16,16 @@ class ResultPage extends React.PureComponent {
 			this.sortedPrices = this.props.location.state.sortedPrices;
 			this.sortedRatings = this.props.location.state.sortedRatings;
 			this.sortedMpg = this.props.location.state.sortedMpg;
-			console.log(this.sortedRatings)
-			console.log(this.sortedMpg)
 		}
 		
 
 		this.state = {
 			currentFilter: this.sortedPrices,
-			booga: "booga"
+			upperPrice: this.sortedPrices[0].MSRP
 		};
 
-		// console.log(this.sortedPrices);
-		const filterRange = [
-			{
-				value: 0,
-				label: `MIN VALUE: 0`,
-			},
-			{
-				value: 50,
-				label: `MIN VALUE: 10000`,
-			},
-		];
-
 		this.updateFilter = this.updateFilter.bind(this)
-		this.actualUpdate = this.actualUpdate.bind(this)
+		this.updatePriceFilter = this.updatePriceFilter.bind(this)
 	}
 
 	updateFilter = (chosenFilter) => {
@@ -51,15 +37,12 @@ class ResultPage extends React.PureComponent {
 		const choosen = values[chosenFilter]
 		this.setState({
 			currentFilter: choosen,
-			booga: "bdsfdsf"
 		});
 	};
 
-	actualUpdate = () => {
-		this.setState({
-			booga: "oompga"
-		})
-	}
+	// updatePriceFilter = (maxPrice) => {
+	// 	console.log(maxPrice);
+	// }
 
 	render() {
 		if (typeof this.props.location.state === "undefined") return <p>404</p>;
@@ -67,12 +50,9 @@ class ResultPage extends React.PureComponent {
 			<div>
 				<CarAppBar/>
 				<h1>Here Is What We Found</h1>
-				<Button onClick={this.actualUpdate}>{this.state.booga}</Button>
-				<FilterDrawer func={this.updateFilter}/>
-				{this.state.currentFilter[0].HORSEPOWER}
+				<FilterDrawer priceFunc={this.updatePriceFilter} func={this.updateFilter} maxAvailPrice={this.sortedPrices[0].MSRP}/>
 				<Grid container>
 					{/* maps each car onto the page */}
-					{console.log(this.state.currentFilter)}
 					{Object.entries(this.state.currentFilter).map(([key, value]) => {
 						return (
 							<Grid item xs={12} key={key}>
